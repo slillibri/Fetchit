69be3e5a354ad85081517834ea1cfa5cf2d36eb7
slillibri <scott.lillibridge@gmail.com>
2011-07-17 13:17:09 -0700

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fetchit::Application.config.secret_token = '2e5fc07a68020da6e4ff19ed7b3c55809877e7ad7dd0c7d1a7d45350d770bfe0b1cdb05da045918573d3bf444e2fb17c37b63493d65b952b9cb09719927296a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fetchit::Application.config.session_store :cookie_store, key: '_fetchit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fetchit::Application.config.session_store :active_record_store
