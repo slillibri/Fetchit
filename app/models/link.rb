94e71084e9d683b5a8cd3e6990fccb9c1088ef1f
slillibri <scott.lillibridge@gmail.com>
2011-07-17 16:25:37 -0700

    Clean up url for only http and https before_save

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,9 +3,21 @@ class Link < ActiveRecord::Base
   validates_presence_of :url, :on => :create, :message => "can't be blank"
   
   before_save :gen_key
+  before_save :clean_url
   belongs_to :user_agent, :class_name => "UserAgent", :foreign_key => "user_agent_id"
   
   private
+  
+  def clean_url
+    uri = URI.parse(self.url)
+    unless uri.scheme == 'http' or uri.scheme == 'https'
+      if uri.host.nil?
+        self.url = "http://" + uri.path
+      else
+        self.url = "http://" + uri.host
+      end
+    end
+  end
   
   def gen_key
     if self.hash.nil?
