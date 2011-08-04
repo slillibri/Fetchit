5c7100022fb518eeb7de2eae6b10be794ec45fed
slillibri <scott.lillibridge@gmail.com>
2011-08-04 15:39:50 -0700

    Fixed colorizer

--- lib/fetch_colorizer.rb
+++ lib/fetch_colorizer.rb
@@ -1,20 +1,20 @@
+require 'net/http'
+
 class FetchColorizer
-  attr_accessor :body, :error
+  attr_accessor :body, :error, :color
   def initialize body
     @body = body
   end
   
   def process
     ## Use pygments to colorize body
-    color = error = ''
-    
     begin
       uri = URI.parse('http://pygments.appspot.com/')
-      request = Net::HTTP.post_form(uri, {'lang' => 'html', 'code' => body})
+      request = Net::HTTP.post_form(uri, {'lang' => 'html', 'code' => @body})
       @color = request.body
     rescue Exception => e
-      @color = res.body
-      @error = "#{e.message}: #{request.body}"
+      @color = @body
+      @error = "#{e.message}"
     end
   end
 end
