a4b5458b47ebf8be195d4a358a81395b711830bb
slillibri <scott.lillibridge@gmail.com>
2011-08-07 10:58:25 -0700

    Ajax form and pagination

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   def index
-    @links = Link.order('updated_at DESC').page
+    @links = Link.order('updated_at DESC').page(params[:page])
   end
 
   def show
@@ -14,8 +14,14 @@ class LinksController < ApplicationController
       @link = Link.new(params[:link].merge(:processed => false, :user_agent => @ua))
       if @link.save
         Resque.enqueue(LinkFetcher, @link.id)
-        redirect_to @link
-      end    
+        respond_to do |format|
+          format.html { redirect_to links_path }
+          format.js do
+            @links = Link.order('updated_at DESC').page(params[:page])
+            render :index
+          end
+        end
+      end
     else
       flash[:error] = "You have exceeded the rate limit, please try again in a little bit"
       redirect_to root_path
