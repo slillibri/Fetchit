2202fa6b58df79055b1b2a4e8dc305941389136e
slillibri <scott.lillibridge@gmail.com>
2011-08-24 16:30:55 -0700

    Add support for showing raw body

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -27,4 +27,9 @@ class LinksController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def raw
+    @link = Link.find(params[:link_id])
+    @headers = @link.processed ? YAML.load(@link.headers) : {}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Fetchit::Application.routes.draw do
   resources :links, :only => [:index, :create, :show] do
     get 'page/:page', :action => :index, :on => :collection
+    get 'raw', :action => :raw
   end
   
   root :to => 'links#index'
